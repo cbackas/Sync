ea39a78df0d508a7d4fb607190810fefc398b374
cbackas <cbackasSE@gmail.com>
2016-12-28 17:54:40 -0600

    Some dopeass nickname syncing fam

--- src/main/java/cback/eventFunctions/NicknameChange.java
+++ src/main/java/cback/eventFunctions/NicknameChange.java
@@ -1,38 +1,35 @@
 package cback.eventFunctions;
 
+import cback.Util;
+import cback.globalChannels;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.NickNameChangeEvent;
 import sx.blah.discord.handle.obj.IGuild;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NicknameChange {
+
+    public static List<String> guilds = Arrays.asList("263120914894422017", "192441520178200577", "256248900124540929", "191589587817070593");
 
     @EventSubscriber
     public void nicknameChangeEvent(NickNameChangeEvent event) {
         IGuild guild = event.getGuild();
-        if (guild.getID().equals("256248900124540929") || guild.getID().equals("256248900124540929")) {
 
-            String oldName = event.getUser().getName();
-            if (event.getOldNickname().isPresent()) {
-                oldName = event.getOldNickname().get();
-            }
+        if (guilds.contains(guild.getID())) {
 
-            String newName = event.getUser().getDisplayName(guild);
-            if (event.getNewNickname().isPresent()) {
-                newName = event.getNewNickname().get();
-            }
+            guilds.stream()
+                    .filter(c -> !c.equals(guild.getID()))
+                    .forEach(c -> {
+                        try {
+                            event.getClient().getGuildByID(c).setUserNickname(event.getUser(), event.getUser().getDisplayName(guild));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
 
-            //Sync nickname changes between servers
-            if (guild.getID().equals("256248900124540929")) {
-                try {
-                    event.getClient().getGuildByID("256248900124540929").setUserNickname(event.getUser(), newName);
-                } catch (Exception ignored) {
-                }
-            } else if (guild.getID().equals("256248900124540929")) {
-                try {
-                    event.getClient().getGuildByID("256248900124540929").setUserNickname(event.getUser(), newName);
-                } catch (Exception ignored) {
-                }
-            }
         }
     }
+
 }
