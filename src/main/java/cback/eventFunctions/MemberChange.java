45ac3f9e5ef80e7b39c79e832cd52c584053fa0f
cbackas <cbackasSE@gmail.com>
2017-01-01 19:15:56 -0600

    Add ban syncing (for future bans)

--- src/main/java/cback/eventFunctions/MemberChange.java
+++ src/main/java/cback/eventFunctions/MemberChange.java
@@ -2,11 +2,15 @@ package cback.eventFunctions;
 
 import cback.SyncBot;
 import cback.Util;
+import cback.globalChannels;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.UserBanEvent;
 import sx.blah.discord.handle.impl.events.UserJoinEvent;
 import sx.blah.discord.handle.impl.events.UserLeaveEvent;
 import sx.blah.discord.handle.obj.IUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MemberChange {
     private SyncBot bot;
@@ -14,6 +18,8 @@ public class MemberChange {
     public MemberChange(SyncBot bot) {
         this.bot = bot;
     }
+
+    public static final List<String> ALL_SERVERS = Arrays.asList("192441520178200577", "256248900124540929", "191589587817070593");
 
     @EventSubscriber
     public void memberJoin(UserJoinEvent event) {
@@ -40,6 +46,20 @@ public class MemberChange {
         //Memberlog message
         Util.sendMessage(bot.getClient().getChannelByID(SyncBot.MEMBERLOG_CHANNEL_ID), "\uD83D\uDD28  " + user.getName() + " was **banned** from " + event.getGuild().getName() + " " + user.mention());
 
+        if (ALL_SERVERS.contains(event.getGuild().getID())) {
+
+            ALL_SERVERS.stream()
+                    .filter(g -> !g.equals(event.getGuild().getID()))
+                    .forEach(g -> {
+
+                        try {
+                            event.getClient().getGuildByID(g).banUser(user, 1);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    });
+        }
     }
 
 }
